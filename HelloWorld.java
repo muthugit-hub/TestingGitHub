595f5aa69822f6600892c52e2d25045ee0850f0e
Vengadesan JB <143320045+VengadesanJB@users.noreply.github.com>
2023-08-31 21:30:29 +0530

    Given Welcome Drinks
    I have added welcome Drinks features

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld{
     System.out.println("Hello World");
     System.out.println("Hello India");
   }
+  public void drinks(){
+  System.out.println("Given WelCome Drinks");
+  }
 
 public static void main(String[] args){
 HelloWorld h = new HelloWorld();
